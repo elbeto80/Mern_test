dfd6f4f7294f0b2ba306ce84c2a4a0e4f5838ee9
Alberto Alvarez <elbeto80@hotmail.com>
2022-07-27 18:37:30 -0500

    Delete task by id

--- Back/task.controller.js
+++ Back/task.controller.js
@@ -3,13 +3,11 @@ import { dbConection } from "./config.js";
 export const getTasks = async (req, res) => {
   const [tasks] = await dbConection.query("select * from tasks");
 
-  res.status(200).json({ tasks });
+  return res.status(200).json({ tasks });
 };
 
 export const getTask = async (req, res) => {
   const { id } = req.params;
-
-  console.log(req.body);
 
   const [task] = await dbConection.query("select * from tasks where id = ?", [
     id,
@@ -18,7 +16,7 @@ export const getTask = async (req, res) => {
   if (task.length == 0)
     return res.status(404).json({ message: "id no encontado" });
 
-  res.status(200).json(task[0]);
+  return res.status(200).json(task[0]);
 };
 
 export const addTask = async (req, res) => {
@@ -28,15 +26,23 @@ export const addTask = async (req, res) => {
     "INSERT INTO tasks (title, description) VALUES (?, ?)",
     [title, description]
   );
-  console.log(result);
 
-  res.status(200).json({ id: result.insertId, title, description });
+  return res.status(200).json({ id: result.insertId, title, description });
 };
 
 export const updateTask = (req, res) => {
   res.send("actualizando");
 };
 
-export const deleteTask = (req, res) => {
-  res.send("eliminando");
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  const [result] = await dbConection.query("delete from tasks where id = ?", [
+    id,
+  ]);
+
+  if (result.affectedRows == 0)
+    return res.status(404).json({ message: "id no encontado" });
+
+  return res.sendStatus(204);
 };
